442b7767dac551a324c53421db6c4742d708faeb
V-G-DOTNET <68254839+V-G-DOTNET@users.noreply.github.com>
2021-03-23 16:36:49 +0400

    Business Layer , Data layer and Interface layer
    Need to include this three library in project solution for logic , data, and property of API

---
+++ DataLayer/ConfiClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using System.Reflection;
+using System.IO;
+
+namespace DataLayer
+{
+	public class ConfigClass
+	{
+		private static string configFileName = "/generated.config";
+		private static string settingsPath = "appSettings/add";
+		private static Configuration Configuration { get; set; }
+
+		public static string GetConnectionString(string key)
+		{
+			return Configuration.AppSettings[key];
+		}
+
+		static ConfigClass()
+		{
+			XmlDocument doc = new XmlDocument();
+			string path = new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath;
+			string rootDir = Path.GetDirectoryName(path);
+			doc.Load(rootDir + configFileName);
+			var root = doc.DocumentElement;
+			var list = root.SelectNodes(settingsPath);
+			Configuration = new Configuration();
+			foreach (var setting in list)
+			{
+				var element = (XmlElement)setting;
+				var key = element.Attributes["key"].Value;
+				var value = element.Attributes["value"].Value;
+				Configuration.AppSettings.Add(key, value);
+			}
+		}
+	}
+
+	public class Configuration
+	{
+		public Dictionary<string, string> AppSettings { get; }
+		public Configuration()
+		{
+			AppSettings = new Dictionary<string, string>();
+		}
+	}
+}
---
+++ InterfaceLayer/SignupIL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace InterfaceLayer
+{
+    public interface SignupIL
+    {
+        #region Properties
+
+        string FirstName { get; set; }
+
+        string LastName { get; set; }
+
+        string Email { get; set; }
+
+        string Password { get; set; }
+
+        string Role { get; set; }
+
+        #endregion
+
+        #region Functions
+        int AddUser();
+        //DataSet CheckLogin();
+        #endregion
+    }
+}
